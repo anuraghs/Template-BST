961b744c7656080696f32debecc8f85768d8376d
anuraghs <77752473+anuraghs@users.noreply.github.com>
2022-08-05 18:20:22 -0700

    Update bst.h

--- bst.h
+++ bst.h
@@ -1,5 +1,5 @@
 // intbst.h
-// Binary search tree header file for CS 24 lab
+// Binary search tree header file 
 
 #ifndef BST_H
 #define BST_H
@@ -18,14 +18,14 @@ class IntBST {
     bool insert(T value);     // insert value; return false if duplicate
     void printPreOrder() const; // prints tree data pre-order to cout
 
-    // 8 METHODS YOU MUST IMPLEMENT in intbst.cpp:
+    
     void printInOrder() const;       // print tree data in-order to cout
     void printPostOrder() const;     // print tree data post-order to cout
     T sum() const;                 // sum of all values
     int count() const;               // count of values
     bool contains(T value) const;  // true if value is in tree
 
-    // THESE ARE HARDER! DO THESE LAST
+    
     T getPredecessor(T value) const;       // returns the predecessor value of the given value or 0 if there is none
     T getSuccessor(T value) const;         // returns the successor value of the given value or 0 if there is none
     bool remove(T value);                    // deletes the Node containing the given value from the tree
@@ -34,7 +34,7 @@ class IntBST {
     struct Node {
 	T info;
 	Node *left, *right, * parent;
-	// useful constructor:
+	
     Node(T v=0) : info(v), left(nullptr), right(nullptr), parent(nullptr) { }
     };
 
@@ -42,7 +42,7 @@ class IntBST {
     Node *root;
 
     // recursive utility functions for use by public methods above
-    Node* getNodeFor(T value, Node* n) const; // IMPLEMENT THIS FIRST: returns the node for a given value or NULL if none exists
+    Node* getNodeFor(T value, Node* n) const; // returns the node for a given value or NULL if none exists
     void clear(Node *n); // for destructor
     bool insert(T value, Node *n); // note overloading names for simplicity
     void printPreOrder(Node *n) const;
@@ -51,7 +51,6 @@ class IntBST {
     T sum(Node *n) const;
     int count(Node *n) const;
 
-    // these should be used by getPredecessor and getSuccessor, and ONE of them should be used by remove
     Node* getSuccessorNode(T value) const;   // returns the Node containing the successor of the given value
     Node* getPredecessorNode(T value) const; // returns the Node containing the predecessor of the given value 
 };
